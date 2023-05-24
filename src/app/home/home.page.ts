e783cedf245c28ca5f090ab2880921ec6cf1cb46
pg <grigolopi@gmail.com>
2023-05-24 14:30:40 +0200

    sistemazione per nuove strutture dati

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -40,9 +40,9 @@ export class HomePage {
   private showPreview = false;
 
   
-  public datiTesta!: PagDati; //dati filtrati per l'intestazione del documento
-  public datiTab!: TabDati; //dati filtrati per il corpo del documento (tabella)
-  public datiPie!: PagDati; //dati filtrati per il piede del documento
+  public datiPagina!: PagDati; //tetsti e immagini da inserire nella pagina
+  public datiTab!: TabDati; //dati per la creazione tabella
+  
 
   public paginaConf!: PdfOption; //configurazione pagina estrapolata dai dati
   public paginaStile!: PdfStyle; //configurazioni stile estrapolata dai dati
@@ -57,116 +57,90 @@ export class HomePage {
     private dati: DatiDocumentoService,
     private dbRequest: ChiamataDBService,
   ) {}
-  //input nome setter
-  set nome(n:string){
-    console.log('setter nome')
-    this.input.nome = n;
-  }
-  //input nome getter
-  get nome(){
-    return this.input.nome
-  }
-  //input nome eventHandler
-  changeName(e: any){
-    const inNome: string = e.target.value;
-    this.nome = (inNome === '' ? '' : (inNome[0].toLocaleUpperCase() + inNome.substring(1)));
-  }
+//############################input frontend###################################
+  // //input nome setter
+  // set nome(n:string){
+  //   console.log('setter nome')
+  //   this.input.nome = n;
+  // }
+  // //input nome getter
+  // get nome(){
+  //   return this.input.nome
+  // }
+  // //input nome eventHandler
+  // changeName(e: any){
+  //   const inNome: string = e.target.value;
+  //   this.nome = (inNome === '' ?
+  //  '' : 
+  // (inNome[0].toLocaleUpperCase() + inNome.substring(1)));
+  // }
 
-  //input tipo doc setter
-  set tipo(t:string){
-    this.input.tipo = t;
-  }
-  //input tipo doc getter
-  get tipo(){
-    return this.input.tipo;
-  }
-  //input tipo doc eventHandler
-  changeTipo(e: any){
-    const tipoDoc = e.target.value;
-    this.input.tipo = tipoDoc;
-  }
-  //input stile - tema setter
-  set tema(i: string){
-    this.input.stile.tema = i;
-  }
+  // //input tipo doc setter
+  // set tipo(t:string){
+  //   this.input.tipo = t;
+  // }
+  // //input tipo doc getter
+  // get tipo(){
+  //   return this.input.tipo;
+  // }
+  // //input tipo doc eventHandler
+  // changeTipo(e: any){
+  //   const tipoDoc = e.target.value;
+  //   this.input.tipo = tipoDoc;
+  // }
+  // //input stile - tema setter
+  // set tema(i: string){
+  //   this.input.stile.tema = i;
+  // }
 
-  //input stile - tema getter
-  get tema(){
-    return this.input.stile.tema;
-  }
+  // //input stile - tema getter
+  // get tema(){
+  //   return this.input.stile.tema;
+  // }
 
-  //input stile - tema change handler
-  changeTema(e: any){
-    this.tema = e.target.value;
-  }
+  // //input stile - tema change handler
+  // changeTema(e: any){
+  //   this.tema = e.target.value;
+  // }
 
-  //preview -> true; mostra l'anteprima nella pagina false non 
-  set preview(yn: boolean){
-    this.showPreview = yn;
-  }
+  // //preview -> true; mostra l'anteprima nella pagina false non 
+  // set preview(yn: boolean){
+  //   this.showPreview = yn;
+  // }
 
-  get preview() {
-    return this.showPreview;
-  }
+  // get preview() {
+  //   return this.showPreview;
+  // }
+//############################fine input frontend##############################
 
   recuperaVisualizza(){
     /**Lancia la chiamata al DB, quindi smista la risposta tra i vari servizi
      * per inizializzare le proprietà */
-    //chiamata DB
-    this.dbRequest.richiestaDati(this.input.nome);
-    const resConf = this.dbRequest.configRaw;
-    const resDati = this.dbRequest.datiRaw;
+    //chiamata DB: 
+    //passo nome, nome della chiave e valore
+    this.dbRequest.templateParams = {
+      templateName: 'Offerta',
+      keyName: 'numPre',
+      keyVal: 103
+    }
+
+    let template = this.dbRequest.templateDoc;
+    console.log('template arrivata alla home:', template);
+
+    
 
     //filtro e assegno dati di config alle proprietà di questa classe
-    this.conf.paginaConfig = resConf;
-    this.paginaConf = this.conf.paginaConfig;
-    
-    this.conf.docStyle = resConf;
-    this.paginaStile = this.conf.docStyle;
-
-    this.conf.tabOption = resConf;
-    this.tabellaOption = this.conf.tabOption;
 
         
     //filtro e assegno dati del contenuto doc alle proprietà di questa classe
-    this.dati.intestazione = resDati;
-    this.datiTesta = this.dati.intestazione;
 
-    this.dati.tabella = resDati;
-    this.datiTab = this.dati.tabella;
-
-    this.dati.piepagina = resDati;
-    this.datiPie = this.dati.piepagina;
-
-    // console.log(
-    //   'Dati documento:',
-    //   this.datiTesta,
-    //   this.datiTab,
-    //   this.datiPie,
-    // );
-    // console.log(
-    //   '\nDati Configurazione: ',
-    //   this.paginaConf,
-    //   this.paginaStile,
-    //   this.tabellaOption
-    // );
-
-    this.preview = true;
   }
 
   generaDoc(){
     /**Presi gli input dalla pagina, richiama il servizio Config
      * per generare gli oggetti di configurazione del documento */
-    let tmpl: string = this.input.tipo != '' ?
-      this.input.tipo :
-      this.dbRequest.configRaw.tipo.toLocaleLowerCase();
-    
-    this.doc.creaDoc(this.paginaConf, tmpl);
 
-    let obj: HTMLObjectElement = document.getElementsByTagName('object')[0];
-    obj.data = this.doc.test();
-    console.log('location hash', obj)
-    
+
   }
 
 }
